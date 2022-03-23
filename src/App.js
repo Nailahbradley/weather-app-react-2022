71e9a0d8bb7f77cf7724392f8a93c326d3e390ab
Nailahbradley <bradleynailah@gmail.com>
2022-03-22 19:39:28 -0500

    Added footer with my Github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This app was coded by Nailah Bradley and is{" "}
+        <a
+          href="https://github.com/Nailahbradley/weather-app-react-2022"
+          target="_blank"
+          rel="noreferrer"
+          className="footer"
+        >
+          {" "}
+          open-sourced on Github{" "}
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
